52bf2ebbe6b4d0c6fc41d2e1a14d3051b042f6e8
evan <97516411+0x3van@users.noreply.github.com>
2022-05-07 17:32:50 +0800

    update #1

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 $(function() {
-    $(window).load(function() {
-      $(this).scrollTop(0);
-      $(".se-pre-con").fadeOut(4000);
-    });
-    window.onbeforeunload = function() {
-      window.scrollTo(0, 0);
-    };
+  $(window).load(function() {
+    $(this).scrollTop(0);
+    $(".se-pre-con").fadeOut(4000);
   });
+  window.onbeforeunload = function() {
+    window.scrollTo(0, 0);
+  };
+});
+
+        var app = document.getElementById('appapp');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+typewriter.typeString('by trollface security')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('in popbob we trust')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('"k-klep-klepto-kle-k-k-k-k-kleptocracy -boe jiden"')
+    .pauseFor(2500)
+    .start();
